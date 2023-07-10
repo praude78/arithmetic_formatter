4698c45040664cf2890f62a6777fbe72930c0d84
Lucio Praude <praude78@gmail.com>
2023-07-10 11:01:00 +0100

    operator + or -

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -7,4 +7,14 @@ def arithmetic_arranger(problems, show_answers=False):
     bottom_line = []
     separator_line = []
     answer_line = []
+    
+    for problem in problems:
+      parts = problem.split()
+
+      operand1 = parts[0]
+      operator = parts[1]
+      operand2 = parts[2]
+
+	if operator not in ['+', '-']:
+      return "Error: Operator must be '+' or '-'."
 
